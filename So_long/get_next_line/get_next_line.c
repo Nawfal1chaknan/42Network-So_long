d8c9c65aff41c0539c106b96bec46920dc1ffa7b
Nawfal1chaknan <61191519+Nawfal1chaknan@users.noreply.github.com>
2023-02-27 20:32:59 +0100

    Add files via upload

---
+++ So_long/get_next_line/get_next_line.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchaknan <nchaknan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 18:47:38 by nchaknan          #+#    #+#             */
+/*   Updated: 2022/11/16 16:26:43 by nchaknan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*firstp(char *str)
+{
+	char	*finals;
+	int		i;
+
+	if (!str)
+		return (0);
+	finals = malloc(ft_strlen(str) + 1);
+	if (!finals)
+		return (0);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		finals[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		finals[i] = str[i];
+		i++;
+	}
+	finals[i] = '\0';
+	return (finals);
+}
+
+char	*lastp(char *str)
+{
+	char	*finals;
+	int		i;
+	int		j;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	finals = malloc(ft_strlen(str) - i + 1);
+	if (!finals)
+		return (0);
+	j = 0;
+	while (str[i])
+		finals[j++] = str[i++];
+	finals[j] = '\0';
+	if (finals[0] == '\0')
+	{
+		free(finals);
+		finals = 0;
+	}
+	free(str);
+	return (finals);
+}
+
+char	*my_stock(int fd, char *stock)
+{
+	char	*buffer;
+	int		r;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (0);
+	r = 1;
+	while (r > 0 && !(ft_strchr(stock, '\n')))
+	{
+		r = read(fd, buffer, BUFFER_SIZE);
+		if (r == -1)
+		{
+			free(buffer);
+			free(stock);
+			stock = 0;
+			return (0);
+		}
+		buffer[r] = '\0';
+		stock = ft_strjoin(stock, buffer);
+	}
+	free(buffer);
+	return (stock);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stock;
+	char		*finalstr;
+
+	if (fd < 0)
+		return (0);
+	stock = my_stock(fd, stock);
+	finalstr = firstp(stock);
+	stock = lastp(stock);
+	return (finalstr);
+}
---
+++ So_long/set_map.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_map.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchaknan <nchaknan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 19:23:07 by nchaknan          #+#    #+#             */
+/*   Updated: 2023/02/25 22:36:24 by nchaknan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	read_map(t_mylist *myList, char **array)
+{
+	int	fd;
+	int	i;
+
+	fd = open("map.ber", O_RDONLY);
+	myList->height = count_lines(fd);
+	fd = open("map.ber", O_RDONLY);
+	i = 0;
+	while (i < myList->height)
+	{
+		array[i] = get_next_line(fd);
+		i++;
+	}
+}
+
+void	identify_map(t_mylist *myList)
+{
+	myList->array = malloc(sizeof(char *) * 1024);
+	read_map(myList, myList->array);
+	myList->width = map_strlen(myList->array[0]);
+	myList->pixels = 48;
+	myList->mlx = mlx_init();
+	myList->window = mlx_new_window(myList->mlx, myList->pixels
+			* myList->width, myList->pixels * myList->height, "So Long");
+	myList->wall = mlx_xpm_file_to_image(myList->mlx, WALL,
+			&myList->pixels, &myList->pixels);
+	myList->bg = mlx_xpm_file_to_image(myList->mlx, BACKGROUND,
+			&myList->pixels, &myList->pixels);
+	myList->player = mlx_xpm_file_to_image(myList->mlx, PLAYER,
+			&myList->pixels, &myList->pixels);
+	myList->coin = mlx_xpm_file_to_image(myList->mlx, COIN,
+			&myList->pixels, &myList->pixels);
+	myList->exit = mlx_xpm_file_to_image(myList->mlx, EXIT,
+			&myList->pixels, &myList->pixels);
+	myList->player_count = 0;
+	myList->coins_count = 0;
+	myList->exit_count = 0;
+	myList->moves = 0;
+	find_player_position(myList);
+}
+
+void	put_images(t_mylist *list, int x, int y)
+{
+	mlx_put_image_to_window(list->mlx, list->window, list->bg,
+		x * list->pixels, y * list->pixels);
+	if (list->array[y][x] == '1')
+		mlx_put_image_to_window(list->mlx, list->window, list->wall,
+			x * list->pixels, y * list->pixels);
+	else if (list->array[y][x] == 'P')
+		mlx_put_image_to_window(list->mlx, list->window, list->player,
+			x * list->pixels, y * list->pixels);
+	else if (list->array[y][x] == 'C')
+		mlx_put_image_to_window(list->mlx, list->window, list->coin,
+			x * list->pixels, y * list->pixels);
+	else if (list->array[y][x] == 'E')
+		mlx_put_image_to_window(list->mlx, list->window, list->exit,
+			x * list->pixels, y * list->pixels);
+	else
+		mlx_put_image_to_window(list->mlx, list->window, list->bg,
+			x * list->pixels, y * list->pixels);
+}
+
+void	draw_map(t_mylist *list)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < list->height)
+	{
+		x = 0;
+		while (x < list->width)
+		{
+			put_images(list, x, y);
+			x++;
+		}
+		y++;
+	}
+}
